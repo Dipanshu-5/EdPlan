2a19c342bbb529c37b17d4424a240bb140db4695
Dipanshu-5 <dipanshukapri25@gmail.com>
2025-12-03 11:43:33 +0530

    added masters json data

--- ChatbotUI/src/services/educationPlanService.js
+++ ChatbotUI/src/services/educationPlanService.js
@@ -1,23 +1,23 @@
 let educationCache = null;
 
 const loadPrograms = async () => {
-  if (educationCache) return educationCache;
-  const response = await fetch('/assets/responses/programdetail.json');
-  if (!response.ok) {
-    throw new Error('Unable to load education plans');
-  }
-  educationCache = await response.json();
-  return educationCache;
+	if (educationCache) return educationCache;
+	const response = await fetch("/public/assets/responses/programdetail.json");
+	if (!response.ok) {
+		throw new Error("Unable to load education plans");
+	}
+	educationCache = await response.json();
+	return educationCache;
 };
 
 export const findProgramPlan = async (programName, universityName) => {
-  const programs = await loadPrograms();
-  return (
-    programs.find(
-      (program) =>
-        program.program === programName && program.university === universityName
-    ) || null
-  );
+	const programs = await loadPrograms();
+	return (
+		programs.find(
+			(program) =>
+				program.program === programName && program.university === universityName
+		) || null
+	);
 };
 
 export const listPrograms = async () => loadPrograms();
